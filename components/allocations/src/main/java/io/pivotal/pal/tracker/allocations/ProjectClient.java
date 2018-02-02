783ee4bceb0291657fd8c8002b1333138a884f65
e050246 <bret.deasy@mastercard.com>
2018-02-02 13:39:43 -0500

    Added service binding for Hystrix

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,18 +1,38 @@
 package io.pivotal.pal.tracker.allocations;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
     private final String registrationServerEndpoint;
+    private final Map<String, ProjectInfo> projectCache;
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations= restOperations;
         this.registrationServerEndpoint = registrationServerEndpoint;
+        this.projectCache = new ConcurrentHashMap<>();
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        String url = getUrl(projectId);
+        ProjectInfo fetchedProjectInfo = restOperations.getForObject(url, ProjectInfo.class);
+
+        projectCache.put(url, fetchedProjectInfo);
+
+        return fetchedProjectInfo;
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        return projectCache.get(getUrl(projectId));
+    }
+
+    private String getUrl(long projectId) {
+        return registrationServerEndpoint + "/projects/" + projectId;
     }
 }
--- components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
+++ components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
@@ -1,18 +1,38 @@
 package io.pivotal.pal.tracker.backlog;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
-    private final String endpoint;
+    private final String registrationServerEndpoint;
+    private final Map<String, ProjectInfo> projectCache;
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
-        this.restOperations = restOperations;
-        this.endpoint = registrationServerEndpoint;
+        this.restOperations= restOperations;
+        this.registrationServerEndpoint = registrationServerEndpoint;
+        this.projectCache = new ConcurrentHashMap<>();
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        String url = getUrl(projectId);
+        ProjectInfo fetchedProjectInfo = restOperations.getForObject(url, ProjectInfo.class);
+
+        projectCache.put(url, fetchedProjectInfo);
+
+        return fetchedProjectInfo;
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        return projectCache.get(getUrl(projectId));
+    }
+
+    private String getUrl(long projectId) {
+        return registrationServerEndpoint + "/projects/" + projectId;
     }
 }
